9b376d2b632f72d9fe66676bda11c064a8c8e395
Ayoze Martin <ayozemt@gmail.com>
2023-07-02 13:08:30 +0200

    Invite and add parent to family.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -119,6 +119,111 @@ router.post("/signup", (req, res, next) => {
     }); // In this case, we send error handling to the error handling middleware.
 });
 
+router.post("/join-family/:familyId", (req, res, next) => {
+  const { familyId } = req.params;
+  const { email, password, name, surname, dni } = req.body;
+
+  if (email === "" || password === "" || name === "" || surname === "" || dni === "") {
+    res.status(400).json({ message: "Debes rellenar todos los campos." });
+    return;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: "Proporciona un email válido." });
+    return;
+  }
+
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    console.log("TEST", passwordRegex.test(password));
+    res.status(400).json({
+      message:
+        "La contraseña debe tener al menos 6 caracteres y contener al menos un número, una letra minúscula y una mayúscula.",
+    });
+    return;
+  }
+
+  const dniRegex = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
+  if (!dniRegex.test(dni)) {
+    res.status(400).json({
+      message: "Proporciona un DNI válido: 8 números y una letra mayúscula.",
+    });
+    return;
+  }
+
+  function dniLetter(dni) {
+    const dniOnlyNumbers = dni.slice(0, 8);
+    const lockup = "TRWAGMYFPDXBNJZSQVHLCKE";
+    const letter = lockup.charAt(dniOnlyNumbers % 23);
+    return letter;
+  }
+
+  if (dniLetter(dni) !== dni.charAt(8)) {
+    res.status(400).json({ message: "Proporciona un DNI válido." });
+    return;
+  } else {
+    console.log("DNI VALIDO");
+  }
+
+  // Check the users collection if a user with the same email already exists
+
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res.status(400).json({ message: "El usuario ya existe." });
+        return;
+      }
+
+      // If email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      // Create the new user in the database
+      // We return a pending promise, which allows us to chain another `then`
+      return User.create({
+        email,
+        password: hashedPassword,
+        name,
+        surname,
+        dni,
+        family: [],
+      });
+    })
+    .then((createdUser) => {
+      const { _id } = createdUser;
+      // Ahora añadimos el usuario a la familia y añadimos su apellido al de la familia
+      return Family.findByIdAndUpdate(
+        familyId,
+        { $push: { parents: _id }, $set: { surname: family.surname + "-" + surname } },
+        { new: true }
+      ).then((updatedFamily) => {
+        // Ahora añadimos la familia al usuario
+        return User.findByIdAndUpdate(
+          _id,
+          { $push: { family: updatedFamily._id } },
+          { new: true }
+        );
+      });
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      // We should never expose passwords publicly
+      const { email, name, _id } = createdUser;
+
+      // Create a new object that doesn't expose the password
+      const user = { email, name, _id };
+
+      // Send a json response containing the user object
+      res.status(201).json({ user: user });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal server error" });
+    }); // In this case, we send error handling to the error handling middleware.
+});
+
 // POST  /auth/login - Verifies email and password and returns a JWT
 router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
