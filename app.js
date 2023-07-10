a84091da883b71b5c3015a1f9ab1a478eab40be5
Ayoze Martin <ayozemt@gmail.com>
2023-07-10 20:31:22 +0200

    Organize routes

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
-const { isAuthenticated } = require("./middleware/jwt.middleware");
-
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -18,22 +16,7 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
-
-const familyRoutes = require("./routes/family.routes");
-app.use("/family", isAuthenticated, familyRoutes);
-
-const childRoutes = require("./routes/child.routes");
-app.use("/child", isAuthenticated, childRoutes);
-
-const vaccineRoutes = require("./routes/vaccine.routes");
-app.use("/vaccines", isAuthenticated, vaccineRoutes);
-
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
-
-const centersRoutes = require("./routes/centers.routes");
-app.use("/centers", isAuthenticated, centersRoutes);
+app.use("/", indexRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
