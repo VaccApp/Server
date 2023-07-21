8358c33c3272122593f0e053a1385e5868f10175
Jon <jon_ander_23@protonmail.com>
2023-07-21 21:52:16 +0200

    dedeploy

--- config/index.js
+++ config/index.js
@@ -22,17 +22,17 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
   // app.use(
   //   cors({
-  //     origin: ["https://vaccapp.netlify.app/", process.env.ORIGIN],
-  //     credentials: true,
+  //     origin: [FRONTEND_URL],
   //   })
   // );
+  app.use(
+    cors({
+      origin: ["https://vaccapp.netlify.app/", process.env.ORIGIN],
+      credentials: true,
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
